21fed6b6543ab25b0046a6be66db7b68a66ee33b
aaaa875 <aaaa875@naver.com>
2014-03-25 15:08:36 +0900

    키워드 중복 체크
    중복된 키워드가 있을 경우 잡아냄

--- keyword_insert.php
+++ keyword_insert.php
@@ -7,6 +7,28 @@ $ip=$_SERVER['REMOTE_ADDR'];
 $title=$_GET['title'];
 $synonyms=$_GET['synonyms'];
 $link=$_GET['link'];
+
+#title이나 synonyms 에 중복된 단어가 있는지 체크
+#나중에 인덱서를 만들어야 함
+$q="SELECT group_concat(keyword_history.title,',',keyword_history.synonyms) as r FROM keyword,keyword_history where keyword.history_id=keyword_history.id";
+$result=mysql_query($q,$db);
+$row=mysql_fetch_array($result);
+$r=$row['r'];
+$tmp=$title.','.$synonyms;
+$tmp=explode(",",$tmp);
+$r=explode(",",$r);
+$tmp2=array_intersect($r,$tmp);
+if(count($tmp2))
+{
+	echo "중복된 타이틀이나 동의어가 존재합니다.<br>";
+	foreach($tmp2 as $w)
+	{
+		echo $w."<br>";
+	}
+	die();
+}
+
+
 $q="INSERT INTO keyword_history (title, synonyms, link, ip) VALUES('$title','$synonyms','$link','$ip');";
 mysql_query($q,$db) or die('error');
 
